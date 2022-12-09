0148f4186cbb31d18c3b3a73606499577fcef0b4
AdrienGiot <adrien.giot@student.uclouvain.be>
2022-12-09 19:43:31 +0100

    aled

--- node.erl
+++ node.erl
@@ -2,30 +2,29 @@
 -compile(export_all).
 
 exec(View, SubsetSize, ExpectedNodes, Id) -> % Should subset size be adapted, were part of nodes to die ?
+	io:format("Hi, I'm node ~p with view ~p\n", [Id, View]),
 	receive
-		{request, Subset, NodeId} ->   
+		{request, _, NodeId} ->   
 			% respond to views sent
-            io:format("receiving a view ~p\n", [View]),
-			NodeId ! {response, sample([], View, SubsetSize, lists:nth(random:uniform(length(View))), {-1,0})},
+            io:format("Hi, I'm node ~p receiving request from ~p\n", [Id, NodeId]),
+			NodeId ! {response, node:sample([], View, SubsetSize, lists:nth(rand:uniform(length(View)), View), {-1,0}), Id},			
 			exec(View, SubsetSize, ExpectedNodes, Id);
-		{response, Subset} ->
-			% Remove entries pointing to P (-> {P,0})
-			% lists:filter(fun ({Node,Age}) -> not lists:member(Elem, B) end, A ).
-			lists:filter(fun ({Node,Age}) -> Node/=Id end, Subset ).
-
-			% or already present entries in P's (-> {P, NP}) view from the subset send by Q. ()
-			% Update P's view with the remaining entries commencing by empty entries of P (add {P1,N1})
-			% and after by replacing entries sent to Q. (replace {P1,N1} by {P1,N2})
-			exec(View, SubsetSize, ExpectedNodes, Id);
+		{response, Subset, NodeId} ->
+			%exec(View, SubsetSize, ExpectedNodes, Id);
+			io:format("Hi, I'm node ~p receiving response from ~p\n", [Id, NodeId]),
+			handle_response(View, SubsetSize, ExpectedNodes, Id, [Node || {Node, _} <- lists:filter(fun ({Node,_}) -> Node/=Id end, Subset )], NodeId, node:get_request_subset(NodeId, ExpectedNodes));
 		period ->
-			handle_period(View,SubsetSize,ExpectedNodes,Id,sample([], View, SubsetSize, lists:nth(random:uniform(length(View))), oldest(-1, -1, View)), oldest(-1, -1, View))
-						
+			io:format("Hi, I'm node ~p having my period\n", [Id]), % CurList, View, R, Candidate, Oldest
+			io:format("arguments :  ~p\n", [oldest(-1, -1, View)]),
+			handle_period(inc(View),SubsetSize,ExpectedNodes,Id,node:sample([], inc(View), SubsetSize, oldest(-1, -1, inc(View)), oldest(-1, -1, inc(View))), oldest(-1, -1, inc(View)));						
 		{timeout, ToCheck} ->
-			case  lists:member(ToCheck, ExpectedNodes) of 
-				true -> % node did not respond in time, remove it of the view					
-					exec(del_in_view(View, ToCheck), SubsetSize, del_in_expected(ExpectedNodes, ToCheck), Id);
-				false ->
-					exec(View, SubsetSize, del_in_expected(ExpectedNodes, ToCheck), Id)
+			io:format("Hi, I'm node ~p checking timeout for ~p\n", [Id, ToCheck]),
+			case  lists:member(ToCheck, [Node || {Node, _} <- ExpectedNodes]) of 
+				true -> % node did not respond in time, remove it of the view	lists:filter(fun ({Node,_}) -> Node/=ToDel end, View).				
+					exec(del_in_view(View, ToCheck), SubsetSize, lists:filter(fun({Node,_}) -> Node/=ToCheck end, ExpectedNodes) , Id);
+				false ->%[[],[{'25',0},{'12',0},{'40',0},{'7',0},{'39',0}],2,{'39',0}]
+					exec(View, SubsetSize, ExpectedNodes, Id)
+			end
 	end.
 
 period(Pid) ->
@@ -44,51 +43,91 @@ oldest(CurOldest, OldestAge, [{Candidate, Age}|T]) ->
 		false -> 
 			oldest(Candidate, Age, T)
 	end;
-oldest(CurOldest, OldestAge, []) ->
+oldest(CurOldest, _, []) ->
 	CurOldest.
 
 % to test : node:sample([], [{'7',4},{'71',4}, {'25',4}], 2, {'71',4}).
-sample(CurList, View, 0, Candidate, Oldest) -> 
+sample(CurList, _, 0, _, _) -> 
 	CurList;
 sample(CurList, View, R, Candidate, Oldest) ->
-	io:format("View : ~p ; CurList ~p\n", [View, CurList]),
-	io:format("Lengths : ~p ; CurList ~p\n", [length(View), length(CurList)]),
-	io:format("Lengths : ~p\n", [length(View)==length(CurList)]),
+	%io:format("View : ~p ; CurList ~p\n", [View, CurList]),
+	%io:format("Lengths : ~p ; CurList ~p\n", [length(View), length(CurList)]),
+	%io:format("Lengths : ~p\n", [length(View)==length(CurList)]),
 	case length(CurList)==length(View) of 
 		true -> % in case view too small 
 			CurList;
 		false ->
-			case lists:member(Candidate, CurList); Candidate==Oldest of 
+			case lists:member(Candidate, CurList) orelse Candidate==Oldest of 
 				true ->
-					sample(CurList, View, R, lists:nth(random:uniform(length(View)), View));
+					node:sample(CurList, View, R, lists:nth(rand:uniform(length(View)), View), Oldest);
 				false ->
-					sample([Candidate|CurList], View, R-1, lists:nth(random:uniform(length(View)), View))
+					node:sample([Candidate|CurList], View, R-1, lists:nth(rand:uniform(length(View)), View), Oldest)
 				end
 		end.
 
-% to test : node:reset_subset([{'7',4},{'71',4}, {'25',4}, {'78',4},{'22',4},{'42',4},{'73',4},{'58',4},{'15',4},{'3',4}], [{'42',4},{'73',4},{'22',4}], []).
-reset_subset([{Node,Age}|T], ChosenOnes, NewView) -> 
-	case lists:member({Node,Age}, ChosenOnes) of
-		true ->
-			reset_subset(T, ChosenOnes, [NewView|{Node,0}]);
-		false ->
-			reset_subset(T, ChosenOnes, [NewView|{Node,Age}])
-		end;
-reset_subset([], ChosenOnes, NewView) ->
-	NewView.
+% to test : node:reset_subset([{'7',4},{'71',4}, {'25',4}, {'78',4},{'22',4},{'42',4},{'73',4},{'58',4},{'15',5},{'3',4}], {'15',5}, []).
+reset_Q([{Node,Age}|T], Oldest, NewView) -> 
+ 	case Node==Oldest of
+ 		true ->
+			(NewView ++ [{Node,0}]) ++ T;
+ 		false ->
+ 			reset_Q(T, Oldest, NewView++[{Node,Age}])
+ 		end;
+reset_Q([], _, NewView) ->
+ 	NewView.
+
+del_in_view(View, ToDel) ->
+	 lists:filter(fun ({Node,_}) -> Node/=ToDel end, View).
 
 handle_period(View, SubsetSize, ExpectedNodes, Id, Subset, Oldest) ->
-	Oldest ! {request, Subset, Id}, % TODO : don't send counts ideally
-	io:format("having a period ~p\n", [View]),
-	timer:send_after(3000, period),
-	timer:send_after(1000, {timeout, Oldest}),
-	exec(inc(reset_subset(View, Subset, [])), SubsetSize, [Oldest | ExpectedNodes]);
+	case length(View)==0 of
+		true -> 
+			node:exec(View, SubsetSize, ExpectedNodes, Id);
+		false ->
+			io:format("Hi, I'm node ~p about to send view to ~p in my period\n", [Id, Oldest]),
+			Oldest ! {request, Subset, Id}, % TODO : don't send counts ideally
+			timer:send_after(3000, period),
+			timer:send_after(1000, {timeout, Oldest}),
+			io:format("Oldest : ~p reset_Q : ~p\n then: ~p", [Oldest, View, node:reset_Q(View, Oldest, [])]),
+			node:exec(node:reset_Q(View, Oldest, []), SubsetSize, [{Oldest,Subset}] ++ExpectedNodes, Id)
+		end.
+
+%Oldest : '3' reset_Q : [{'12',17},{'3',17},{'14',16},{'7',16},{'10',16}]
+% then: [{'12',17},{'3',17},{'14',16},{'7',16},{'10',16}]
+%lists:uniq(fun({X, _}) -> X end, lists:merge(Subset,View))
+handle_response(View, SubsetSize, ExpectedNodes, Id, RespSubset, ToAck, RequestSubset) ->
+	% get_new_view(View, RespSubset, lists:filter(fun ({Node,_}) -> not lists:member(Node, RespSubset) end, RequestSubset))
+	node:exec(node:get_new_view(View, node:get_to_add(RequestSubset, RespSubset), node:get_to_del(RequestSubset, RespSubset)), SubsetSize, lists:filter(fun ({Node,_}) -> Node/=ToAck end, ExpectedNodes), Id ).
+
+get_to_del(Req, Resp) ->
+	[ToR || {ToR, _} <-  lists:filter(fun ({Node,_}) -> not lists:member(Node, Resp) end, Req)].
+
+get_to_add(Req, Resp) -> %[Node || {Node, _} <- ExpectedNodes]
+	io:format("hein\n"),
+	io:format("~p, ~p\n", [Req, Resp]),
+	lists:filter(fun (Node) -> not lists:member(Node, [ReqNode || {ReqNode, _} <- Req]) end, Resp).
+%lists:uniq(fun({X, _}) -> X end, RespSubset++View)
+get_new_view(View, ToAdd, ToDelete) ->
+	% todo : check whether cache is full
+	lists:uniq(fun({X, _}) -> X end, lists:filter(fun ({Node,_}) -> not lists:member(Node, ToDelete) end,View) ++ lists:map(fun (Node) -> {Node, 0} end, ToAdd)).
+
+% node:get_new_view(View, RespSubset, get_to_del(RequestSubset, RespSubset)), SubsetSize, lists:filter(fun ({Node,_}) -> Node/=ToAck end, ExpectedNodes), Id ).
+% [{'1',3},{'2',4},{'3',5}, {'4',6}]
+% [{'1',3},{'2',4}]
+% ['2','3']
+get_request_subset(ToAck, ExpectedNodes) -> 
+	case lists:dropwhile(fun({Node,_}) -> Node/=ToAck end, ExpectedNodes) of 
+		[] -> 
+			nosubset;
+		[{_,ToR}|_] ->
+			ToR
+		end.
 
 % https://stackoverflow.com/questions/10318156/how-to-create-a-list-of-1000-random-numbers-in-erlang
 init_view(N, Id) -> 
 	make_view(N div 10, [], N,  list_to_atom(integer_to_list(Id)),  list_to_atom(integer_to_list(rand:uniform(N)))).
 
-make_view(0, CurList, N, Id, Candidate) -> CurList;
+make_view(0, CurList, _, _, _) -> CurList;
 make_view(R, CurList, N, Id, Candidate) ->	
 	case Id==Candidate of 
 	true ->
@@ -102,7 +141,7 @@ make_view(R, CurList, N, Id, Candidate) ->
 		end
 	end.
 
-start_nodes(0, Cluster, N) -> Cluster;
+start_nodes(0, Cluster, _) -> Cluster;
 start_nodes(Cur, Cluster, N) ->
     Node = list_to_atom(integer_to_list(Cur)),
 	NodePid = spawn(?MODULE, exec, [init_view(N, Cur), N div 20, [], Node]),
@@ -111,6 +150,6 @@ start_nodes(Cur, Cluster, N) ->
     start_nodes(Cur - 1, [Node | Cluster], N).
 
 main() ->
-	start_nodes(100, [], 100),
+	start_nodes(20, [], 20),
     %timer:send_interval(300, list_to_atom(integer_to_list(rand:uniform(5))), view),
 	ok.
